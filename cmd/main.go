1250411e91711694f5e4810659e65ddecf4ec241
Jyolando <pokhilov@codabra.org>
2023-06-23 13:42:12 +0300

    fix: remove errors init from main

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
+	"github.com/jyolando/test-ozon-go/internal/entities"
 	"github.com/jyolando/test-ozon-go/internal/handlers"
 	"github.com/jyolando/test-ozon-go/pkg/helpers"
 )
 
-const (
-	RunError = "run error"
-)
-
 func main() {
-	//var errInfo string
-
 	logger := helpers.NewLogger()
 
 	logger.Info("starting")
 	if server, err := handlers.NewServer(logger); err != nil {
 		logger.Error(err)
 	} else if err := server.Run(); err != nil {
-		logger.Fatal(RunError)
+		logger.Fatal(entities.RunError)
 	}
-
 }
